8bf8c50b48b516859baf050c67ba811ec368dd52
Luís Henrique <lhfreya@gmail.com>
2021-09-02 17:04:03 -0300

    Consumo de personagens API Marvel

---
+++ Exercícios Python/pymarvel.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+def consumomarvel(dicionario):
+
+    
+    #try:
+    
+        req = requests.get('https://gateway.marvel.com/v1/public/characters?limit=100&ts=1609890812920&apikey={SUAPUBLICKEY}&hash={SUAHASH}')
+        dicionario = json.loads(req.text)
+        
+        return dicionario['data']['results']
+        
+        
+        
+
+    #except:
+
+        #print('Erro ao estabelecer conexão!')
+        
+
+
+def personagemwiki (Character, nome_personagem):
+    
+    for personagem in Character :
+        if (personagem["name"] == nome_personagem):
+            print(personagem['description'])
+            break
+
+
+sair = False
+while not sair:
+
+    pesquisa = input("Digite o nome de um personagem ou sair para fechar:")
+
+    if pesquisa == ('sair'):
+        sair = True
+        print('Até mais!') 
+
+    else:
+        Character = consumomarvel(pesquisa)
+        if Character == 'null':
+           print('Personagem não encontrado')
+        
+        else:
+            personagemwiki(Character, pesquisa)
+            
